3bb9dd876af1a3e7c03ad4d4d795ad16e75f59d9
viacheslav <viacheslav.g@5ly.co>
2022-06-09 19:34:59 +0300

    add buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Calendar from "./components/Calendar";
 import Footer from "./components/Footer";
 import { useState } from "react";
 
+const DAYS_IN_MONTH = 30;
+
 function Page({ weddingDate }) {
   return (
     <div className={s.wrapper}>
@@ -14,18 +16,19 @@ function Page({ weddingDate }) {
   );
 }
 
-
 function App() {
-    const [weddingDate, setWeddingDate] = useState(0);
-
-    console.log(weddingDate);
+    const [weddingDate, setWeddingDate] = useState(26);
 
     return (
       <div>
         <div className={s.pageWrapper}>
           {Array.from({ length: 12 }).map(() => <Page weddingDate={weddingDate}/>)}
         </div>
-        <input className={s.input} onChange={(e) => setWeddingDate(Number(e.target.value))}/>
+          <div className={s.inputWrapper}>
+              <div className={s.button} onClick={() => setWeddingDate((prev) => prev > 1 ? prev - 1 : prev)}>-</div>
+              <input className={s.input} value={weddingDate} onChange={(e) => setWeddingDate(Number(e.target.value))}/>
+              <div className={s.button} onClick={() => setWeddingDate((prev) => prev < DAYS_IN_MONTH ? prev + 1 : prev)}>+</div>
+          </div>
       </div>
     );
 }
